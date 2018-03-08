a8975b60e93047b26d674dcff7bf97affc9c23d1
utkuglsvn <utku.glsvn@gmail.com>
2018-03-08 23:52:27 +0300

    Add files via upload

---
+++ java_oop/AnimalInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_oop;
+
+/**
+ *
+ * @author glsvn
+ */
+
+
+//baseclass
+public interface AnimalInterface {
+    public void live();
+    public void reproductive();
+}
---
+++ java_oop/Carnivora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_oop;
+
+/**
+ *
+ * @author glsvn
+ */
+public class Carnivora {
+    public void eat(String a)
+    {
+        System.out.println(a);
+    }
+    //inner class polymorphism,
+
+   
+}
